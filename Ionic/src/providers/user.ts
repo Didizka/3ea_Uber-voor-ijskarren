d405e3ee3f26c7bbddf96fd6543b92e8f246d6ec
sanjeyan Kanagarasa <s092595@ap.be>
2017-11-08 09:53:08 +0100

    poort aangepast in ionic

--- Ionic/src/providers/user.ts
+++ Ionic/src/providers/user.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Customer} from "../Models/customer";
 
@@ -9,17 +9,23 @@ export class UserProvider {
   constructor(public http: Http) {
 
   }
+  private headers: Headers = new  Headers({
+    'Accept': 'application/json'
+  });
   getAllUsers(){
-    return this.http.delete('http://localhost:52468/api/users/7');
+    return this.http.delete('http://localhost:9000/api/users/7');
   }
   createAccount(user: JSON){
     console.log(user);
-    return this.http.post('http://localhost:52468/api/users', user);
+    return this.http.post('http://localhost:9000/api/users', user);
   }
 
   login(email: string, user: JSON){
     console.log(user);
-    return this.http.post('http://localhost:52468/api/users/' + email, user);
+    return this.http.post('http://localhost:9000/api/users/' + email, user, {headers: this.headers });
+  }
+  getUser(email: string){
+    return this.http.get('http://localhost:9000/api/users/' + email);
   }
 
 }
