8f12e0867d304c4f4eba3bb7703452e2962e7d43
Chingiz Mizambekov <chinjka_m@hotmail.com>
2017-11-13 19:40:14 +0100

    -Moved list-driver module related stuff to apart module and import it in app.module -Added plugin for geolocation on android -Added code to center the map on the phone

--- Ionic/src/pages/list-drivers/list-drivers.module.ts
+++ Ionic/src/pages/list-drivers/list-drivers.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ListDriversPage } from './list-drivers';
+import { Geolocation } from '@ionic-native/geolocation';
+import { AgmCoreModule } from  '@agm/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +10,13 @@ import { ListDriversPage } from './list-drivers';
   ],
   imports: [
     IonicPageModule.forChild(ListDriversPage),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAIsmJrEeHefwTuNe4q94adKczIaEcA2AI'
+    }),
   ],
+  providers: [ Geolocation ],
+
+  // needed to bind lat/lng attributets of agm-map
+  schemas:  [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class ListDriversPageModule {}
--- Ionic/src/pages/list-drivers/list-drivers.ts
+++ Ionic/src/pages/list-drivers/list-drivers.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Driver} from "../../Models/driver";
 import {UserProvider} from "../../providers/user";
+import { Geolocation } from '@ionic-native/geolocation';
 
 //@IonicPage()
 @Component({
@@ -11,14 +12,17 @@ import {UserProvider} from "../../providers/user";
 export class ListDriversPage implements OnInit{
   zoom: number = 10;
   title: string = 'Uber voor ijskarren';
-  lat: number;
-  lng: number;
+  lat: number = 51.2217865;
+  lng: number = 4.461651;
   drivers: Driver[]= [];
+  distance: number = 5;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private userProvider: UserProvider) {
+              private userProvider: UserProvider,
+              private geolocation: Geolocation) {
   }
+
   ngOnInit(){
     this.setCurrentPosition();
     this.userProvider.getDriversLocation().subscribe(
@@ -34,14 +38,28 @@ export class ListDriversPage implements OnInit{
     this.navCtrl.setRoot('SigninPage');
   }
 
+
+  // Does not work with live reload
   setCurrentPosition() {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position);
-        this.lat = position.coords.latitude;
-        this.lng = position.coords.longitude;
-      });
-    }
+    console.log('geolocation activated');
+    this.geolocation.getCurrentPosition({maximumAge:0, timeout:5000, enableHighAccuracy:true}).then(position => {
+      console.log(position);
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+      this.zoom = 8;
+    }).catch((err) => {
+      console.log(err);
+    });
   }
+    // if ('geolocation' in navigator) {
+    //   navigator.geolocation.getCurrentPosition((position) => {
+    //     console.log(position);
+    //     this.lat = position.coords.latitude;
+    //     this.lng = position.coords.longitude;
+    //   });
+    // } else {
+    //   console.log('no geolocation found');
+    // }
+  // }
 
 }
