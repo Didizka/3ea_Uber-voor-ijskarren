4c7c845a9d8b4c1387981be38cd9d07e99613d5c
Chingiz Mizambekov <chinjka_m@hotmail.com>
2017-11-17 17:11:03 +0100

    -Changed google maps to javascript instead of AGM -Added markers and infowindows -Enlarged the map view

--- Ionic/src/pages/list-drivers/list-drivers.ts
+++ Ionic/src/pages/list-drivers/list-drivers.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {Driver} from "../../Models/driver";
-import {UserProvider} from "../../providers/user";
+import { Driver } from "../../Models/driver";
+import { UserProvider } from "../../providers/user";
 import { Geolocation } from "@ionic-native/geolocation";
+
+declare var google;
 
 //@IonicPage()
 @Component({
@@ -10,16 +12,22 @@ import { Geolocation } from "@ionic-native/geolocation";
   templateUrl: 'list-drivers.html',
 })
 export class ListDriversPage implements OnInit{
-  zoom: number = 10;
+  // MAP
+  lat: number = 51.22179;
+  lng: number =  4.461666;  
+  distance: number = 5;
+  zoom: number = 11;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  // APP
   title: string = 'Uber voor ijskarren';
-  //lat: number = 51.2217865;5
-  lat: number = 51.221791;
-  //lng: number = 4.461651;lat: number = 51.2217865;51.221791, 4.461666
-  lng: number = 4.461666;
+
+  // DRIVERS
   drivers: Driver[]= [];
   driversInZone: Driver[]= [];
 
-  distance: number = 5;
+  
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -28,81 +36,150 @@ export class ListDriversPage implements OnInit{
   }
 
   ngOnInit(){
-    this.setCurrentPosition();
 
     this.userProvider.getDriversLocation().subscribe(
       data => {
-        this.drivers = data;
-        this.checkRangeOfDrivers(5);
+        this.drivers = data;       
+        this.checkRangeOfDrivers(this.distance); 
+        this.showMap();         
       },
       err => {
         console.log(err);
       });
   }
-  onRangeChange(event: any){
-    switch (event.value){
-      case 10:
-        this.checkRangeOfDrivers(4);
-        break;
-      case 11:
-        this.checkRangeOfDrivers(3);
-        break;
-      case 12:
-        this.checkRangeOfDrivers(2);
-        break;
-      case 13:
-        this.checkRangeOfDrivers(1);
-        break;
-      default:
-        this.checkRangeOfDrivers(5);
+
+  showMap() {
+    this.getUserPosition();
+    
+        let mapOptions = {
+          center: new google.maps.LatLng(this.lat, this.lng),
+          zoom: this.zoom,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          scrollwheel: false,
+          draggable: false
+        }
+    
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        this.addUserMarker();
+        this.addDriversMarkers();
+  }
+
+  addUserMarker() {
+      let userMarker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: this.map.getCenter(),
+        label: 'U'
+      });
+  
+      let content = "<p>Dit is jouw huidige locatie!</p>";          
+      let infoWindow = new google.maps.InfoWindow({
+        content: content
+      });
+  
+      google.maps.event.addListener(userMarker, 'click', () => {
+        infoWindow.open(this.map, userMarker);
+      });
+  }
+
+  addDriversMarkers() {
+    for (let driver of this.driversInZone) {
+      // console.log(driver);
+      let driversLocation = new google.maps.LatLng(driver.location.latitude, driver.location.longitude)
+      let driverMarker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: driversLocation,
+        label: driver.userID.toString()
+      });
+
+      let content = "<p>Driver ID: <b>" + driver.userID +"</b>, " + driver.firstName + " " + driver.lastName + "</p>";          
+      let infoWindow = new google.maps.InfoWindow({
+        content: content
+      });
+  
+      google.maps.event.addListener(driverMarker, 'click', () => {
+        infoWindow.open(this.map, driverMarker);
+      });
+
     }
   }
+
+  
+  // Does not work with live reload
+  getUserPosition() {
+    // console.log('geolocation activated');
+    let options = {
+      enableHighAccuracy: false
+    };
+    this.geolocation.getCurrentPosition(options).then(position => {
+      // console.log(position);
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+    }).catch(err => {
+      console.log('Position error: ' + err.message);
+    });
+  }
+
+  onRangeChange(event: any){
+    this.zoom = event.value;
+    this.map.setZoom(this.zoom);
+    switch (event.value){
+      case 12:
+        this.distance = 4;
+        break;
+      case 13:
+        this.distance = 3;
+        break;
+      case 14:
+        this.distance = 2;
+        break;
+      case 14:
+        this.distance = 1;
+        break;
+      default:
+        this.distance = 5;        
+    }
+
+    this.checkRangeOfDrivers(this.distance);
+  }
+
   onLogout(){
     this.navCtrl.setRoot('SigninPage');
   }
 
 
-  // Does not work with live reload
-  setCurrentPosition() {
-    console.log('geolocation activated');
-    this.geolocation.getCurrentPosition({maximumAge:0, timeout:5000, enableHighAccuracy:true}).then(position => {
-      console.log(position);
-      this.lat = position.coords.latitude;
-      this.lng = position.coords.longitude;
-      this.zoom = 8;
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
 
   calculateDistance(lat2, lon2){
-    let R = 6371e3; // metres
+    let R = 6371e3; // meters
     let φ1 = this.toRadians(this.lat);
     let φ2 = this.toRadians(lat2);
-    let Δφ = this.toRadians((lat2-this.lat));
-    let Δλ = this.toRadians((lon2-this.lng));
+    let Δφ = this.toRadians((lat2 - this.lat));
+    let Δλ = this.toRadians((lon2 - this.lng));
 
-    let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+    let a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
       Math.cos(φ1) * Math.cos(φ2) *
-      Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     // in meter
     let d = R * c;
     //in km
-    return d/1000;
+    return d / 1000;
     //console.log("Distance is: "+d/1000 + "km")
   }
 
   checkRangeOfDrivers(range: number){
     this.driversInZone.splice(0);
-    console.log("Range: " +range + "km");
+    console.log("Range: " + range + "km");
     for(let driver of this.drivers){
+      // console.log(driver.location);
       let distance = this.calculateDistance(driver.location.latitude, driver.location.longitude);
-      if(distance < range){
+      if(distance <= range){
         this.driversInZone.push(driver);
       }
     }
+    console.log(this.driversInZone);
   }
 
   toRadians(angle) {
